17f10967ce4ce605f5a09bf9a0f4363fc915181e
tsukano22170292 <133092772+tsukano22170292@users.noreply.github.com>
2023-08-29 15:48:23 +0900

    SE追加

--- Assets/scripts/audio_c.cs
+++ Assets/scripts/audio_c.cs
@@ -3,21 +3,32 @@ using UnityEngine;
 public class audio_c : MonoBehaviour
 {
     public AudioSource seSource;
+    public AudioSource cancelSource;
+    public AudioSource recycleSource;
+    public AudioSource shopSource;
+
     // Start is called before the first frame update
-    void Start()
+    public void sePlay()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void sePlay(string seName)
-    {
-        seSource.clip = Resources.Load<AudioClip>(seName);
+        seSource.clip = Resources.Load<AudioClip>("kettei");
         seSource.Play();
+    }
+
+    public void cancelPlay()
+    {
+        cancelSource.clip = Resources.Load<AudioClip>("cancel");
+        cancelSource.Play();
+    }
+
+    public void recyclePlay()
+    {
+        recycleSource.clip = Resources.Load<AudioClip>("recycle");
+        recycleSource.Play();
+    }
+
+    public void shopPlay()
+    {
+        shopSource.clip = Resources.Load<AudioClip>("shop");
+        shopSource.Play();
     }
 }
